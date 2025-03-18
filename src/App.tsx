66ae0f5c2c89f92ed971b83234c9609cf617fd93
Piero Friedrich <pierofriedrich@yahoo.com.br>
2025-03-17 23:59:48 -0700

    add styling, logo and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import Translator from "./components/Translator";
+import TransparentBackground from "./images/transparent-background.png";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkMobile();
+
+    window.addEventListener("resize", checkMobile);
+
+    // Clean up
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div
+      className="min-h-screen flex items-center justify-center bg-slate-400"
+      style={{
+        backgroundImage: `url(${TransparentBackground})`,
+        backgroundSize: isMobile ? "70%" : "40%",
+        backgroundPosition: "top",
+        backgroundRepeat: "repeat-x",
+      }}
+    >
       <Translator />
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        display: ["Poppins", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+      colors: {
+        color1: "#21cbca",
+      },
+    },
+  },
+  plugins: [],
+};
